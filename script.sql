6893eb4a81fb2d4f698fc064fdc41040588f900f
camila de medeiros <camifet@hotmail.com>
2021-11-23 23:04:52 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -4,134 +4,135 @@ Use agencia;
 
 CREATE TABLE pessoa(
     idPessoa INT NOT NULL,
-    Nome VARCHAR(100) NOT NULL,
-    Sexo VARCHAR(45) NULL,
-    DataNascimento date NULL,
-    Endereco VARCHAR(45) NULL,
-    Email VARCHAR(45) NULL,
+    nome VARCHAR(100) NOT NULL,
+    sexo VARCHAR(45) NULL,
+    data_nascimento date NULL,
+    endereco VARCHAR(45) NULL,
+    email VARCHAR(45) NULL,
     viagem_idViagem INT NOT NULL,
     PRIMARY KEY(idPessoa)
     );
     
-CREATE TABLE telefonepessoa (
-    Telefone VARCHAR(45) NOT NULL,
+CREATE TABLE telefone_pessoa (
+    telefone VARCHAR(45) NOT NULL,
     pessoa_idPessoa INT NOT NULL,
-    PRIMARY KEY (Telefone , pessoa_idPessoa)
+    PRIMARY KEY (telefone , pessoa_idPessoa)
      );
     
 CREATE TABLE viagem (
     idViagem INT NOT NULL,
-    Nome VARCHAR(45) NULL,
-    DataInicial datetime(6) NOT NULL,
-    DataFinal datetime(6) NOT NULL,
+    nome VARCHAR(45) NULL,
+    data_inicial datetime(6) NOT NULL,
+    data_final datetime(6) NOT NULL,
     PRIMARY KEY (idViagem)
      );
      
 CREATE TABLE local (
-    idlocal INT NOT NULL,
-    Nome VARCHAR(45) NOT NULL,
+    idLocal INT NOT NULL,
+    nome VARCHAR(45) NOT NULL,
     viagem_idViagem INT NOT NULL,
-    PRIMARY KEY (idlocal)
+    PRIMARY KEY (idLocal)
      );
      
 CREATE TABLE pontos_turisticos (
-    idpontos INT NOT NULL,
-    Nome VARCHAR(45) NOT NULL,
-    Horario VARCHAR(45) NULL,
-    Custo DECIMAL(8,2) NULL,
-    Faixa INT NULL, 
-    tipopontos_idtipoponto INT NOT NULL,
-    local_idlocal INT NOT NULL,
-    PRIMARY KEY (idpontos)
+    idPontos INT NOT NULL,
+    nome VARCHAR(45) NOT NULL,
+    horario VARCHAR(45) NULL,
+    custo DECIMAL(8,2) NULL,
+    faixa INT NULL, 
+    tipo_pontos_idTipo INT NOT NULL,
+    local_idLocal INT NOT NULL,
+    PRIMARY KEY (idPontos)
      );
      
-CREATE TABLE tipopontos (
-    idtipoponto INT NOT NULL,
-    Descricao VARCHAR(45) NOT NULL,
-    PRIMARY KEY (idtipoponto)
+CREATE TABLE tipo_pontos (
+    idTipo INT NOT NULL,
+    descricao VARCHAR(45) NOT NULL,
+    PRIMARY KEY (idTipo)
      );
      
               
-CREATE TABLE telefoneponto (
-    Telefone VARCHAR(45) NOT NULL,
-    pontos_turisticos_idpontos INT NOT NULL,
-    PRIMARY KEY (Telefone , pontos_turisticos_idpontos)
+CREATE TABLE telefone_ponto (
+    telefone VARCHAR(45) NOT NULL,
+    pontos_turisticos_idPontos INT NOT NULL,
+    PRIMARY KEY (telefone , pontos_turisticos_idPontos)
      );
    
 CREATE TABLE calendario (
-    idcalendario INT NOT NULL,
-    Descricao VARCHAR(100) NULL,
+    idCalendario INT NOT NULL,
+    descricao VARCHAR(100) NULL,
     data_inicial DATETIME NULL,
     data_final DATETIME NULL,
     viagem_idViagem INT NOT NULL,
-    pontos_turisticos_idpontos INT NOT NULL,
-    PRIMARY KEY (idcalendario)
+    pontos_turisticos_idPontos INT NOT NULL,
+    PRIMARY KEY (idCalendario)
      );
      
 CREATE TABLE despesa (
-    iddespesa INT NOT NULL,
+    idDespesa INT NOT NULL,
     data_hora DATETIME NULL,
     link VARCHAR(100) NULL,
     email VARCHAR(100) NULL,
     arquivo VARCHAR(100) NULL,
     viagem_idViagem INT NOT NULL,
-    tipodespesa_idtipodespesa INT NOT NULL,
-    PRIMARY KEY (iddespesa)
+    tipo_despesa_idTipo INT NOT NULL,
+    PRIMARY KEY (idDespesa)
      );
      
-CREATE TABLE tipodespesa (
-    idtipodespesa INT NOT NULL,
-    Descricao VARCHAR(100) NULL,
-    PRIMARY KEY (idtipodespesa)
+CREATE TABLE tipo_despesa (
+    idTipo INT NOT NULL,
+    descricao VARCHAR(100) NULL,
+    PRIMARY KEY (idTipo)
      );
      
 CREATE TABLE necessidade (
-    idnecessidade INT NOT NULL,
-    Descricao VARCHAR(100) NULL,
-    PRIMARY KEY (idnecessidade)
+    idNecessidade INT NOT NULL,
+    descricao VARCHAR(100) NULL,
+    PRIMARY KEY (idNecessidade)
      );     
      
 CREATE TABLE necessidade_has_pessoa (
-    necessidade_idnecessidade INT NOT NULL,
+    necessidade_idNecessidade INT NOT NULL,
     pessoa_idPessoa INT NOT NULL,
-    PRIMARY KEY (necessidade_idnecessidade, pessoa_idPessoa)
+    PRIMARY KEY (necessidade_idNecessidade, pessoa_idPessoa)
      );   
      
 CREATE TABLE necessidade_has_pontos_turisticos (
-    necessidade_idnecessidade INT NOT NULL,
-    pessoa_turisticos_idpontos INT NOT NULL,
-    PRIMARY KEY (necessidade_idnecessidade, pessoa_turisticos_idpontos)
+    necessidade_idNecessidade INT NOT NULL,
+    pessoa_turisticos_idPontos INT NOT NULL,
+    PRIMARY KEY (necessidade_idNecessidade, pessoa_turisticos_idPontos)
      );   
 
 ALTER TABLE pessoa
 ADD FOREIGN KEY (viagem_idViagem) REFERENCES viagem(idViagem);
 
-ALTER TABLE telefonepessoa
+ALTER TABLE telefone_pessoa
 ADD FOREIGN KEY (pessoa_idPessoa) REFERENCES pessoa(idPessoa);
 
 ALTER TABLE local
 ADD FOREIGN KEY (viagem_idViagem) REFERENCES viagem(idViagem);
 
 ALTER TABLE pontos_turisticos
-ADD FOREIGN KEY (tipopontos_idtipoponto) REFERENCES tipopontos(idtipoponto),
+ADD FOREIGN KEY (tipo_pontos_idtipo) REFERENCES tipo_pontos(idTipo),
 ADD FOREIGN KEY (local_idlocal) REFERENCES local(idlocal);
 
-ALTER TABLE telefoneponto
-ADD FOREIGN KEY (pontos_turisticos_idpontos) REFERENCES pontos_turisticos(idpontos);
+ALTER TABLE telefone_ponto
+ADD FOREIGN KEY (pontos_turisticos_idpontos) REFERENCES pontos_turisticos(idPontos);
 
 ALTER TABLE calendario
 ADD FOREIGN KEY (viagem_idViagem) REFERENCES viagem(idViagem),
-ADD FOREIGN KEY (pontos_turisticos_idpontos) REFERENCES pontos_turisticos(idpontos);
+ADD FOREIGN KEY (pontos_turisticos_idPontos) REFERENCES pontos_turisticos(idPontos);
 
 ALTER TABLE despesa
 ADD FOREIGN KEY (viagem_idViagem) REFERENCES viagem(idViagem),
-ADD FOREIGN KEY (tipodespesa_idtipodespesa) REFERENCES tipodespesa(idtipodespesa);
+ADD FOREIGN KEY (tipo_despesa_idTipo) REFERENCES tipo_despesa(idTipo);
 
 ALTER TABLE necessidade_has_pessoa
-ADD FOREIGN KEY (necessidade_idnecessidade) REFERENCES necessidade(idnecessidade),
+ADD FOREIGN KEY (necessidade_idNecessidade) REFERENCES necessidade(idNecessidade),
 ADD FOREIGN KEY (pessoa_idPessoa) REFERENCES pessoa(idPessoa);
 
 ALTER TABLE necessidade_has_pontos_turisticos
-ADD FOREIGN KEY (necessidade_idnecessidade) REFERENCES necessidade(idnecessidade),
-ADD FOREIGN KEY (pessoa_turisticos_idpontos) REFERENCES pontos_turisticos(idpontos);
+ADD FOREIGN KEY (necessidade_idNecessidade) REFERENCES necessidade(idNecessidade),
+ADD FOREIGN KEY (pessoa_turisticos_idPontos) REFERENCES pontos_turisticos(idPontos);
+
 
